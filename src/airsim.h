4f3c94cab1cefec2c7869dfb9143b650a85dd60c
rektdie <rektdie@gmail.com>
2026-06-22 23:13:40 +0200

    included assimp for object loading

--- src/airsim.h
+++ src/airsim.h
@@ -3,8 +3,11 @@
 
 #pragma once
 
+#include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include <iostream>
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
 
 // TODO: Reference additional headers your program requires here.
